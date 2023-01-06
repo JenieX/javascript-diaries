686508d6f6bc934a8a930d1c1125d8084949d52d
JenieX <sozorablack@gmail.com>
2023-01-06 07:01:36 +0300

    📝 Update array splice method examples

--- content/global-objects/array/splice/no-delete-count.js
+++ content/global-objects/array/splice/no-delete-count.js
@@ -1,16 +1,20 @@
 /**
- * If deleteCount is omitted, or if its value is greater than or equal to
- * the number of elements after the position specified by start, then all
- * the elements from start to the end of the array will be deleted.
- * However, if you wish to pass any itemN parameter, you should pass Infinity
- * as deleteCount to delete all elements after start, because an explicit
- * undefined gets converted to 0.
+ * Unexplicit undefined value for deleteCount will be treated as Infinity.
+ *
+ * If the value of deleteCount is greater than or equal to the number of
+ * elements after the position specified by start, then all the elements
+ * from start to the end of the array will be deleted.
+ * However, if you wish to add any items, you should pass explicit Infinity
+ * as deleteCount value to delete all elements after start.
+ *
+ * Note: an explicit undefined value for deleteCount gets converted to 0.
  */
 
 const numbers = [1, 2, 3, 4, 5, 6, 7];
 
 // const result = numbers.splice(1);
-const result = numbers.splice(1, Infinity, 10);
+const result = numbers.splice(1, Infinity);
+// const result = numbers.splice(1, Infinity, 10);
 console.log(result);
 
 console.log(numbers);
--- content/global-objects/array/splice/undefined-delete-count.js
+++ content/global-objects/array/splice/undefined-delete-count.js
@@ -4,8 +4,8 @@
 
 const numbers = [1, 2, 3, 4, 5, 6, 7];
 
-// const result = numbers.splice(1, 0);
-const result = numbers.splice(1, undefined);
+const result = numbers.splice(1, 0);
+// const result = numbers.splice(1, undefined);
 console.log(result);
 
 console.log(numbers);
